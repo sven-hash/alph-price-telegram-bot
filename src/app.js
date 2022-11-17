3e021e84b602175348d0807f659a86bdca5b4f06
lowzyyy <peratheking@aol.com>
2022-11-17 16:01:42 +0100

    Added new command for longer stats

--- src/app.js
+++ src/app.js
@@ -2,19 +2,61 @@ import fetch from "node-fetch";
 import { Telegraf } from "telegraf";
 import "./env.js";
 
-const API_URL = "https://api.coingecko.com/api/v3";
+const COINGECKO_API_URL = "https://api.coingecko.com/api/v3";
+const ALEPHIUM_API_URL = "https://backend-v18.mainnet.alephium.org";
+const ONE_MILLION = 1_000_000;
 
-const getCoinData = async () => {
+const fetchData = async (url, errorMsg) => {
   try {
-    const response = await fetch(`${API_URL}/coins/alephium`);
-
-    if (!response.ok) throw new Error(`ERROR FETCH: ${response.status}`);
-
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`${errorMsg}: ${response.status}`);
     const data = await response.json();
-    const marketData = data.market_data;
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getSupply = async (typeOfSupply) => {
+  try {
+    const supply = await fetchData(
+      `${ALEPHIUM_API_URL}/infos/supply/${typeOfSupply}`,
+      "ERROR FETCH ALEPHIUM"
+    );
+    if (supply > ONE_MILLION) return (supply / ONE_MILLION).toFixed(1);
+    return supply;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getMarketData = async () => {
+  try {
+    const data = await fetchData(
+      `${COINGECKO_API_URL}/coins/alephium`,
+      "ERROR FETCH COINGECKO"
+    );
+    return data.market_data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const reservedSupply = async () => {};
+const getCoinData = async (long = false) => {
+  try {
+    // GET MARKET DATA FROM COINGECKO
+    const marketData = await getMarketData();
     const volume = new Intl.NumberFormat("en-EN").format(
       marketData.total_volume.usd
     );
+    // GET CURR SUPPLY FROM ALPH BACKEND
+    const currentSupply = await getSupply("circulating-alph");
+    // GET RESERVED SUPPLY FROM ALPH BACKEND
+    const reservedSupply = await getSupply("reserved-alph");
+    // GET MARKET CAP
+    const marketCap = (currentSupply * marketData.current_price.usd).toFixed(1);
+    // COMBINE ALL INTO ONE DATA
     const coinData = {
       currentPrice: marketData.current_price.usd.toFixed(4),
       change1H:
@@ -25,8 +67,22 @@ const getCoinData = async () => {
       low24: marketData.low_24h.usd.toFixed(4),
       volume: volume,
     };
+
+    if (long)
+      return (
+        `💲Price: $${coinData.currentPrice}\n` +
+        `📈L: $${coinData.low24} | H: $${coinData.high24}\n` +
+        `⏳1H: ${coinData.change1H}%\n` +
+        `⏳24H: ${coinData.change24H}%\n` +
+        `⏳7D: ${coinData.change7D}%\n` +
+        `📊Volume: $${coinData.volume}\n` +
+        `🔓Circulating supply: ${currentSupply}M\n` +
+        `🔐Reserved supply: ${reservedSupply}M\n` +
+        `💰Market cap: $${marketCap}M\n`
+      );
+
     return (
-      `💰Price: $${coinData.currentPrice}\n` +
+      `💲Price: $${coinData.currentPrice}\n` +
       `📈L: $${coinData.low24} | H: $${coinData.high24}\n` +
       `⏳1H: ${coinData.change1H}%\n` +
       `⏳24H: ${coinData.change24H}%\n` +
@@ -40,10 +96,19 @@ const getCoinData = async () => {
   }
 };
 
-const bot = new Telegraf(process.env.TOKEN);
+const bot = new Telegraf(process.env.TOKEN_TEST);
 
 bot.command("p", async (ctx) => {
   const coinData = await getCoinData();
+
+  // Explicit usage
+  // await ctx.telegram.sendMessage(ctx.message.chat.id, coinData);
+
+  // Using context shortcut
+  await ctx.sendMessage(coinData);
+});
+bot.command("plong", async (ctx) => {
+  const coinData = await getCoinData(true);
 
   // Explicit usage
   // await ctx.telegram.sendMessage(ctx.message.chat.id, coinData);
